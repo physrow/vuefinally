a8797fcbceac4f26f6f8a9033a0838ff3b14d05f
Alexander <physrow@gmail.com>
2020-11-13 06:41:16 +0700

    UPDATEEEEE

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="App">
+    <Main/>
+  </div>
+</template>
+
+<script> // in script import "namepage" from ... @Directory . Next import library for use vue-decorator
+import Main from "@/components/Main";
+import { Vue, Component } from 'vue-property-decorator';
+
+// @Components { name of components
+@Component({
+  components: {
+    Main,
+  }
+})
+export default class App extends Vue {} // default
+</script>
---
+++ src/components/Main.vue
@@ -0,0 +1,104 @@
+<script>
+import { Component, Vue } from "vue-property-decorator";
+@Component
+export default class Main extends Vue {
+  checked(name, second_name, phone_number, age, gender,month) {
+    return name && second_name && phone_number && age && gender && month;
+  }
+  check = false;
+  first_name = "";
+  second_name  = "";
+  number = "";
+  age = null;
+  gender = "";
+  validator_word = /[A-Z-a-z]/;
+  month = null;
+  now = new Date().getMonth() + 1;
+
+  captcha(month){
+    if (month !==  this.now){
+      return true;
+    }
+  }
+  name(name) {
+    if (name !== "" && name.length < 30) {
+        for (let i = 0; i < name.length; i++) {
+          if (!this.validator_word.test(name[i])) {
+            return false;
+          }
+        }
+        return true;
+    }else return false;
+  }
+
+  get Data() {
+    return {
+      first_name: this.first_name,
+      second_name: this.second_name,
+      gender: this.gender,
+      age: this.age,
+      number: this.number
+    };
+  }
+
+
+  phone_number(number) {
+    if (number.length === 11 && (number[0] === "8" || number[0] === "7")){
+      return true;
+    }
+  }
+  years(age) {
+    return age < 100;
+  }
+  choice(gender) {
+    return gender !== "";
+  }
+}
+</script>
+
+<template>
+  <div class="Main">
+    <h1>Tinder</h1>
+    <div class="gender">
+      <label>
+        <input type="radio" v-model="gender" value="Female"/>
+      </label>
+      <label>Female</label>
+      <label>
+        <input type="radio" v-model="gender" value="Male"/>
+      </label>
+      <label>Male</label>
+      <label>
+        <input type="radio" v-model="gender" value="Animal"/>
+      </label>
+      <label>Animal</label>
+    </div>
+    <div class="text_in">
+      <label>
+        <input type="text" placeholder="Name ENG < 30" v-model="first_name" class="Main"/>
+      </label>
+    </div>
+    <div class="one">
+      <label>
+        <input type="text" placeholder="Second name ENG< 30" v-model="second_name" class="form_input"/>
+      </label>
+    </div>
+    <div class="one">
+      <label>
+        <input type="number" placeholder="Age < 100" v-model="age" class="form_input"/>
+      </label>
+    </div>
+    <div class="one">
+      <label>
+        <input type="number" placeholder="Number (start 7 or 8, length 11)" v-model="number" class="form_input"/>
+      </label>
+    </div>
+      <div class="one">
+      <label>
+        <input type="number" placeholder="NUMBER OF THIS MONTH?" v-model="month" class="form_input"/>
+      </label>
+    </div>
+    <button type="submit" class="form_button" v-on:click="check = checked(name(first_name), name(second_name), phone_number(number), years(age), choice(gender),captcha(month))">Registration</button>
+    <pre v-if="check">You are registered</pre>
+  </div>
+</template>
